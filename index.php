58f646314e16d4aa030f67181733b85663736070
hendro <hendro.elyeser@gmail.com>
2022-09-16 14:41:31 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, aspernatur similique totam magni architecto quidem quaerat illum quos? Pariatur similique eligendi consequatur! Rerum ducimus explicabo vel minus. Est, ut maxime.</p>
+    <ul>
+        <li>hendro ganteng buanget</li>
+    </ul>
 </body>
 </html>
